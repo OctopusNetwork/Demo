cb6c0a06497a5cb98329f4127e03cef68c435719
mason-ocnet <mason-ocnet>
2018-09-19 07:54:57 +0800

    add datagram

---
+++ Mobile/Android/ocmrpc/app/src/main/java/io/ocnet/ocmrpc/datagram/Config.java
@@ -0,0 +1,7 @@
+package io.ocnet.ocmrpc.datagram;
+
+import java.net.InetSocketAddress;
+
+public abstract class Config {
+    public InetSocketAddress ServerAddress;
+}
---
+++ Mobile/Android/ocmrpc/app/src/main/java/io/ocnet/ocmrpc/datagram/Socks5Protocol.kt
@@ -0,0 +1,35 @@
+package io.ocnet.ocmrpc.datagram
+
+import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+
+/**
+ * Created by owen on 18-3-9.
+ */
+class Socks5Protocol {
+    companion object {
+        fun handshake(destAddress: InetSocketAddress, cryptor: ICrypt): ByteBuffer {
+            val encryptor: ICrypt = cryptor
+            val buffer = ByteBuffer.allocate(1024)
+
+            buffer.clear()
+            // https://shadowsocks.org/en/spec/protocol.html
+
+            buffer.put(0x03.toByte())//domain
+            val domainBytes = destAddress.hostName.toByteArray()
+            buffer.put(domainBytes.size.toByte())//domain length;
+            buffer.put(domainBytes)
+            buffer.putShort(destAddress.port.toShort())
+            buffer.flip()
+
+            val _header = ByteArray(buffer.limit())
+            buffer.get(_header)
+
+            buffer.clear()
+            buffer.put(encryptor.encrypt(_header))
+            buffer.flip()
+
+            return buffer
+        }
+    }
+}
